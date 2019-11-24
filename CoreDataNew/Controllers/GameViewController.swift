324067e450c1f2ea775f6bbce25ed33ca10f4729
StephanGuingor <stephan.guingor04@gmail.com>
2019-11-24 12:21:21 -0600

    setup, to everyone ready up

--- CoreDataNew/Controllers/GameViewController.swift
+++ CoreDataNew/Controllers/GameViewController.swift
@@ -14,12 +14,14 @@ import MultipeerConnectivity
 
 class GameViewController: UIViewController {
     //MARK: READY UP PHASE
+    
+    ///Manages the state of the game, if its set to true it means that the game is currently or about to be executed
     var playerIsReady:Bool = false
     
     //Variables para debug sin Handler
-//    var peerID: MCPeerID!
-//    var mcSession: MCSession!
-//    var mcAdvertiserAssistant: MCAdvertiserAssistant!
+    //    var peerID: MCPeerID!
+    //    var mcSession: MCSession!
+    //    var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
     
     var messageToSend: String!
@@ -42,7 +44,9 @@ class GameViewController: UIViewController {
     ///Will change to true when everyone is ready
     var playersReady:Bool = false
     ///Contains information to manage the turns
-    var playerIndexAndPeerID:[Int:MCPeerID]?
+    var playerIndexAndPeerID:[Int:MCPeerID] = [:]
+    ///Dictionary of the players connected and their ready state
+    var playersAndReadyStatus:[MCPeerID:Bool] = [:]
     ///This struct is in charge of sending all the data across devices
     var dataToSendAsJSON: dataToJSON?
     
@@ -52,31 +56,18 @@ class GameViewController: UIViewController {
         UserDefaults.standard.set(56, forKey: "score")
         let getVar  = UserDefaults.standard.double(forKey: "score")
         print(getVar)
+        
         //ReadyUpButtonSetUp
         
         settingTheButton()
         
+        //Initial set up
         appDelegate = UIApplication.shared.delegate as? AppDelegate
         appDelegate?.mpcHandler.initialSetUp()
         
-        //Is joining to session?
-//        peerID = MCPeerID(displayName: UIDevice.current.name)
-//        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
-//        mcSession.delegate = self
-        
-//        showConnections()
-        
-        
+      
         //Check si el usuario hizo click en el button de join o de host
-        if join ?? false{
-            joinSession2()
-        }else{
-            hostSession2()
-            
-            ///Adding hosting device to the connected players set
-            playersConnected.insert(appDelegate!.mpcHandler.mcSession.myPeerID)
-            print("Welcome \(appDelegate!.mpcHandler.mcSession.myPeerID) to your session!")
-        }
+            joinOrHost()
         
         
         //Observers
@@ -86,20 +77,22 @@ class GameViewController: UIViewController {
         ///This observer is in charge of handling change in state notifications, adn running desired function
         NotificationCenter.default.addObserver(self, selector: #selector(peerChangedStateNotification), name: NSNotification.Name(rawValue: "MPC_DidChangeStateNotification"), object: nil)
     }
-//MARK: JSON FUNCTIONS
+    
+    
+    //MARK: JSON FUNCTIONS
     ///Function designed to convert objects into data , so it can be sent
-    func encodeToJSON(name: String, index: Int, ready: Bool, cardID: String, targetPeer: Int) -> Data{
+    func encodeToJSON(_ object: dataToJSON) -> Data{
         let encoder = JSONEncoder()
-        let messageToSend = dataToJSON(name: name, index: index, ready: ready, cardID: cardID, targetPeer: targetPeer)
+        let messageToSend = object
         
         do {
             let dataToSend = try encoder.encode(messageToSend)
-
+            
             return dataToSend
             //For Debug Use
-//            if let jsonString = String(data: dataToSend, encoding: .utf8) {
-//                print(jsonString)
-//            }
+            //            if let jsonString = String(data: dataToSend, encoding: .utf8) {
+            //                print(jsonString)
+            //            }
         } catch {
             print(error.localizedDescription)
         }
@@ -107,69 +100,96 @@ class GameViewController: UIViewController {
     }
     
     ///Function designed to recieve information from other peers, by decoding data into objects
-    func decodeToJSON(_ data: Data){
+    func decodeToJSON(_ data: Data) -> dataToJSON{
         
         do{
             
-        let peerMessageRecieved = try JSONDecoder().decode(dataToJSON.self, from: data)
-        print(peerMessageRecieved.name)
+            let peerMessageRecieved = try JSONDecoder().decode(dataToJSON.self, from: data)
+            print(peerMessageRecieved.name)
+            return peerMessageRecieved
         }catch{
             print("JSON Decode Failed \(error.localizedDescription)")
         }
-       
+        //default
+        return dataToJSON(name: "", index: -1, ready: false, cardID: nil, targetPeer: nil)
     }
     
     
     //MARK: Game SetUp
     
-
+    //MARK:GS: Getting Ready
     @IBAction func toggleReady(_ sender: Any) {
         
-        playerIsReady = !playerIsReady
+        playerIsReady.toggle()
+        
         switch playerIsReady{
         case true:
             readyUpButton.backgroundColor = UIColor.green
             
             readyUpButton.setAttributedTitle(NSAttributedString(string: "Ready", attributes: fontAttributes), for: .normal)
-            
+            //updates the status in local dictionary
+            playersAndReadyStatus[appDelegate!.mpcHandler.mcSession.myPeerID] = true
+            //sending information about ready status
+            sendStateIsReadyOrUnReady(true)
             
         case false:
             readyUpButton.backgroundColor = UIColor.red
             
             readyUpButton.setAttributedTitle(NSAttributedString(string: "Un-Ready", attributes: fontAttributes), for: .normal)
+            
+            playersAndReadyStatus[appDelegate!.mpcHandler.mcSession.myPeerID] = true
+            sendStateIsReadyOrUnReady(true)
         }
     }
     
-    func sendStateIsReadyOrUnReady(_ message: String){
+    ///Sends an object from dataToJSON that everyone will recieve, the important piece is if the player is ready
+    func sendStateIsReadyOrUnReady(_ ready: Bool){
         
-//        let message = dataToJSON(name: appDelegate?.mpcHandler.mcSession.myPeerID.displayName ?? "No-Name", index: <#T##Int#>, ready: <#T##Bool#>, cardID: <#T##String?#>, targetPeer: <#T##Int?#>)
-        messageToSend = "\(appDelegate?.mpcHandler.peerID.displayName ?? "none")"
+        let message = dataToJSON(name: appDelegate?.mpcHandler.mcSession.myPeerID.displayName ?? "No-Name", index: -1, ready: ready, cardID: nil, targetPeer: nil)
+        
+        let msgData = encodeToJSON(message)
+        
+        
+        do{
+            ///Encoded object will be sent to every player
+            try appDelegate!.mpcHandler.mcSession.send(msgData, toPeers: appDelegate!.mpcHandler.mcSession.connectedPeers, with: .unreliable)
             
-            guard let msg = messageToSend.data(using: .utf8,
-                                               allowLossyConversion: true) else {return}
-            do{
-                try appDelegate!.mpcHandler.mcSession.send(msg, toPeers: appDelegate!.mpcHandler.mcSession.connectedPeers, with: .unreliable)
-                
-            }catch{
-                print(error.localizedDescription)
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+    
+    ///Function will check for everyone state, reeady and unready, if it matches the players connected count, game starts.
+    func checkIfEveryoneIsReady() -> Bool{
+        var readyCount = 0
+        
+        for element in playersAndReadyStatus{
+            if element.value{
+                readyCount += 1
             }
         }
+        
+        //if every connected player is ready it will return true
+        if playersConnected.count >= 2{
+            return playersConnected.count == readyCount
+        }
+        return false
+    }
     
-
     ///Esta funcion es solo para debugging, muestra una alerta para unirse o hacer host
     @IBAction func sendMCPeerIDToPlayersInSession(_ sender: UIButton){
-       
-          messageToSend = "\(appDelegate?.mpcHandler.peerID.displayName ?? "none")"
-          
-          guard let msg = messageToSend.data(using: .utf8,
-                                             allowLossyConversion: true) else {return}
-          do{
-              try appDelegate!.mpcHandler.mcSession.send(msg, toPeers: appDelegate!.mpcHandler.mcSession.connectedPeers, with: .unreliable)
-              
-          }catch{
-              print(error.localizedDescription)
-          }
-      }
+        
+        messageToSend = "\(appDelegate?.mpcHandler.peerID.displayName ?? "none")"
+        
+        guard let msg = messageToSend.data(using: .utf8,
+                                           allowLossyConversion: true) else {return}
+        do{
+            try appDelegate!.mpcHandler.mcSession.send(msg, toPeers: appDelegate!.mpcHandler.mcSession.connectedPeers, with: .unreliable)
+            
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
     
     func everyOneReadyUp(){
         
@@ -184,35 +204,35 @@ class GameViewController: UIViewController {
     }
     
     ///Displays an alert controller, for user to choose between joining or hosting, this should only be used for debug purposes
-        func showConnections(){
-            let alert = UIAlertController(title: "Available Connections", message: "Choose the best one", preferredStyle: .alert)
-            
-            let hostAction  = UIAlertAction(title: "host session", style: .default, handler: hostSession)
-            
-            let joinAction = UIAlertAction(title: "join session", style: .default, handler: joinSession)
-            
-            let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
-            
-            alert.addAction(hostAction)
-            alert.addAction(joinAction)
-            alert.addAction(cancel)
-            present(alert, animated: true, completion: nil)
-        }
+    func showConnections(){
+        let alert = UIAlertController(title: "Available Connections", message: "Choose the best one", preferredStyle: .alert)
+        
+        let hostAction  = UIAlertAction(title: "host session", style: .default, handler: hostSession)
+        
+        let joinAction = UIAlertAction(title: "join session", style: .default, handler: joinSession)
+        
+        let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(hostAction)
+        alert.addAction(joinAction)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
     
-        ///Funcion de hosting para alert view, solo para debug
-        func hostSession(sender: UIAlertAction?){
-    //        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "chat", discoveryInfo: nil, session: mcSession)
-    //        mcAdvertiserAssistant.start()
-        }
+    ///Funcion de hosting para alert view, solo para debug
+    func hostSession(sender: UIAlertAction?){
+        //        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "chat", discoveryInfo: nil, session: mcSession)
+        //        mcAdvertiserAssistant.start()
+    }
     
-        ///Funcion de join para alert view, solo para debug
-        func joinSession(sender: UIAlertAction?){
-    //        let browser = MCBrowserViewController(serviceType: "chat", session: mcSession)
-    //        browser.delegate = self
-    //        present(browser, animated: true) {
-    //            print("browser presented")
-    //        }
-        }
+    ///Funcion de join para alert view, solo para debug
+    func joinSession(sender: UIAlertAction?){
+        //        let browser = MCBrowserViewController(serviceType: "chat", session: mcSession)
+        //        browser.delegate = self
+        //        present(browser, animated: true) {
+        //            print("browser presented")
+        //        }
+    }
     
     //MARK:Set Up Button
     func settingTheButton(){
@@ -227,25 +247,55 @@ class GameViewController: UIViewController {
         
     }
     
-
+    
     
     //MARK: MPC Handler
     
-     ///Brings up MCBrowser Controller in order to join to an existing session
-     func joinSession2(){
-         appDelegate?.mpcHandler.joinSession(delegate: self, from: self)
-     }
+    //Check si el usuario hizo click en el button de join o de host
+    func joinOrHost(){
+        
+        if join ?? false{
+            joinSession2()
+        }else{
+            hostSession2()
+            ///Adding hosting device to the connected players set
+            playersConnected.insert(appDelegate!.mpcHandler.mcSession.myPeerID)
+            print("Welcome \(appDelegate!.mpcHandler.mcSession.myPeerID) to your session!")
+        }
+    }
+    ///Brings up MCBrowser Controller in order to join to an existing session
+    func joinSession2(){
+        appDelegate?.mpcHandler.joinSession(delegate: self, from: self)
+    }
     
-     /// Via de MCAdvertiserAssistant, it advertises current session  to other devices
-     func hostSession2(){
-         appDelegate?.mpcHandler.hostSession()
-     }
-     
+    /// Via de MCAdvertiserAssistant, it advertises current session  to other devices
+    func hostSession2(){
+        appDelegate?.mpcHandler.hostSession()
+    }
+    
     ///Function runned by observer in charge of data notifications
     @objc func recievedDataNotification(_ notification: Notification){
         let data = notification.userInfo!["data"] as? Data
+        let peer = notification.userInfo!["peerID"] as? MCPeerID
+        
+        //this variable is for debug purposes, this will name of device.
         let recievedString = String(bytes: data!, encoding: .utf8)
-
+        
+        let decodedData = decodeToJSON(data!)
+        
+        
+        ///This is to manage all the ready states in the game in order to begin, once the game is runnning; this function will not keep running
+        if !playersReady{
+            //sets the peer and state in a dictionary
+            playersAndReadyStatus[peer!] = decodedData.ready
+            
+            
+            //if everyone is ready, game will start
+            if checkIfEveryoneIsReady(){
+                playersReady = true
+            }
+        }
+        
         readyUpButton.setAttributedTitle(NSAttributedString(string: recievedString!, attributes: fontAttributes), for: .normal)
         print("Data recieved")
         
@@ -271,7 +321,7 @@ class GameViewController: UIViewController {
             break
         }
     }
-///This function return will tell if we should keep adding players to the connected players set or if we have reached a maximum capacity.
+    ///This function return will tell if we should keep adding players to the connected players set or if we have reached a maximum capacity.
     func checkIfAvailableSpace(_ peer: MCPeerID) -> Bool{
         if playersConnected.count < 4{
             return true
@@ -283,10 +333,10 @@ class GameViewController: UIViewController {
             }
             //Stops advertising th host, we do a nil check in order to see who is the host. Non-Host will be nil
             if appDelegate!.mpcHandler.mcAdvertiserAssistant != nil{
-               appDelegate?.mpcHandler.mcAdvertiserAssistant!.stop()
+                appDelegate?.mpcHandler.mcAdvertiserAssistant!.stop()
                 print("\(appDelegate!.mpcHandler.mcSession.myPeerID.displayName) has stopped advertising your game")
             }
-        return false
+            return false
         }
     }
     
--- CoreDataNew/Models/sendingDataModel.swift
+++ CoreDataNew/Models/sendingDataModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MultipeerConnectivity
 
 class dataToJSON: Codable {
     
